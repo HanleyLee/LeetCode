8e93a0205474d83c587c7b5dadff1b18070a9370
李鸿磊 <hanley.lei@gmail.com>
2020-10-16 15:59:07 +0800

    daily 2020-10-16

---
+++ Solutions/21.合并两个有序链表.swift
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=swift
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (64.53%)
+ * Likes:    1323
+ * Dislikes: 0
+ * Total Accepted:    389.7K
+ * Total Submissions: 603.9K
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        
+    }
+}
+// @lc code=end
+
